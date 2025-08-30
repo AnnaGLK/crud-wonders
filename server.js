131a67638c49acf7adfce6cd400ae4d01e8ba7a1
Anna Glikin <glikin.anna@gmail.com>
2025-08-31 00:51:32 +0300

    crud-wonders

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 const wonders = [
-    { name: "Mount Everest", location: "Nepal", visited: false },
-    { name: "Grand Canyon", location: "Arizona", visited: false },
-    { name: "Botanical Gardens", location: "Singapore", visited: true },
-    { name: "Pantheon", location: "Greece", visited: false },
-    { name: "Colosseum", location: "Italy", visited: true }
-]
+  { name: "Mount Everest", location: "Nepal", visited: false },
+  { name: "Grand Canyon", location: "Arizona", visited: false },
+  { name: "Botanical Gardens", location: "Singapore", visited: true },
+  { name: "Pantheon", location: "Greece", visited: false },
+  { name: "Colosseum", location: "Italy", visited: true },
+];
 
-app.get('/wonders', function (req, res) {
-    res.send(wonders)
-})
 
-const port = 1337 //because why not
+
+app.get("/wonders", function (req, res) {
+  console.log("GET /wonders was called");
+  res.send(wonders);
+  
+});
+
+app.post("/wonders", (req, res) => {
+  const newWonder = { ...req.body, visited: false };
+  wonders.push(newWonder);
+  console.log("POST /wonders:", newWonder);
+  res.status(201).send(newWonder);
+});
+
+const port = 1337; //because why not
 app.listen(port, function () {
-    console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
